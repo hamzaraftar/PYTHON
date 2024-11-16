c4848c846ac1ef6d54185c598e9b646c50024da6
Hamza Asghar <hamzaraftar765@gmail.com>
2024-11-16 09:23:21 -0800

    day 2 project complete

---
+++ 2_day/index.py
@@ -0,0 +1,6 @@
+# print(len('hamza'))
+# print(type("hamza"))
+# print(type(123))
+# print(type(True))
+# print(type(1.5))
+print(45 / 5.5 **2)
---
+++ 2_day/project.py
@@ -0,0 +1,7 @@
+print("Wellcome to the tip calculater : ")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would like to give? 10 12 15"))
+poeple = int (input("How many people to split the bill? "))
+billWithTip = tip  / 100 * bill + bill
+
+
