7d363b825199cb5c722ca046c02190422d9ac06e
Hamza Asghar <hamzaraftar765@gmail.com>
2024-11-18 06:00:05 -0800

    day 3 if else

---
+++ 3_day/index.py
@@ -0,0 +1,15 @@
+# print("Wellcome to rollercoster !")
+# height  = int(input("What is your hight in cm ?"))
+# if height > 120:
+#     print("You can ride the rollercoster  ")
+# else :
+#     print("Sory you can grow to ride ")
+
+number = int(input("What is number you want to check ?"))
+
+result  = number % 2
+
+if result == 0:
+    print("Number is even ")
+else :
+    print("Number is odd ")    
