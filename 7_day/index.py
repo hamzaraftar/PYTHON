15a465fe8ddbe47682d36bf9c891f1999945dfe1
Hamza Asghar <hamzaraftar765@gmail.com>
2024-11-20 08:46:55 -0800

    complete half project

--- 7_day/index.py
+++ 7_day/index.py
@@ -2,10 +2,20 @@ import random
 word_list = ["aardvark" , "baboon" , "cammel"]
 choise_word = random.choice(word_list)
 print(choise_word)
+
+placeholder = ""
+word_length = len(choise_word)
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)   
+
 guess = input("Guess a latter: ").lower()
+
+display = ""
 
 for latter in choise_word:
     if latter == guess:
-        print("Right") 
+        display += latter
     else:
-        print("Wrong")
+        display += "_"
+print(display)        
