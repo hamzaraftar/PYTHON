580aed4712def23f2a9d513534475c8fb247753c
Hamza Asghar <hamzaraftar765@gmail.com>
2024-11-20 08:04:32 -0800

    complete taske 2

--- 7_day/index.py
+++ 7_day/index.py
@@ -2,4 +2,8 @@ import random
 word_list = ["aardvark" , "baboon" , "cammel"]
 choise_word = random.choice(word_list)
 print(choise_word)
+guess = input("Guess a latter: ").lower()
 
+for latter in choise_word:
+    if latter == guess:
+        print(latter)    
