5cc558db42efad4f3c15d557f5de7cdf4a1a8d76
Hamza Asghar <hamzaraftar765@gmail.com>
2024-11-21 06:11:12 -0800

    position arg and keyword arg

--- 8_day/index.py
+++ 8_day/index.py
@@ -1,9 +1,16 @@
-def greet():
-    print("Hello")
-    print("how do you do ")
-# greet()
-# fuction that allow input
+# def greet():
+#     print("Hello")
+#     print("how do you do ")
+# # greet()
+# # fuction that allow input
 
-def greet_with_name(name):
-    print(f"hello {name}")
-greet_with_name("hamza")    
+# def greet_with_name(name):
+#     print(f"hello {name}")
+# greet_with_name("hamza")    
+def greet_with(name ,location):
+    print(f"Hello {name}")
+    print(f"your location is {location}")
+
+# greet_with("hellow",'lahore')    
+# keywork argument
+greet_with( name="hamza",location="Lahore")    
