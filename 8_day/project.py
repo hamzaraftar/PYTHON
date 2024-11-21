53b838e2fbc51cfea5fed912ea694cbf5ed8a11b
Hamza Asghar <hamzaraftar765@gmail.com>
2024-11-21 09:03:29 -0800

    project task 1

---
+++ 8_day/project.py
@@ -0,0 +1,14 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+direction = input("Type 'encode' to encrypt , type decode to 'decrypy' \n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(originalText,shiftAmount):
+    cipherText = ""
+    for latter in originalText:
+        shiftedPosition = alphabet.index(latter) + shiftAmount
+        shiftedPosition %= len(alphabet)
+        cipherText += alphabet[shiftedPosition]
+    print(f'Here is encoded result: {cipherText}')
+
+encrypt(originalText = text , shiftAmount = shift)
