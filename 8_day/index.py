bd0bb2ecd0c91e9b9dc6751d8fb84b426dcaeb4e
Hamza Asghar <hamzaraftar765@gmail.com>
2024-11-21 05:35:46 -0800

    funtion with input

---
+++ 8_day/index.py
@@ -0,0 +1,9 @@
+def greet():
+    print("Hello")
+    print("how do you do ")
+# greet()
+# fuction that allow input
+
+def greet_with_name(name):
+    print(f"hello {name}")
+greet_with_name("hamza")    
