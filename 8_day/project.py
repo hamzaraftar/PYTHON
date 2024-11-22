6b59cc3d30fa23d8500ed39df98dcaf2fd8ec88c
Hamza Asghar <hamzaraftar765@gmail.com>
2024-11-22 04:18:02 -0800

    completing day 8 project

--- 8_day/project.py
+++ 8_day/project.py
@@ -4,16 +4,37 @@ direction = input("Type 'encode' to encrypt , type decode to 'decrypy' \n").lowe
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 # to encrypt message
-def encrypt(originalText , shiftAmount):
-    cipherText = ""
-    for latter in originalText:
-        shiftedPosition = alphabet.index(latter) + shiftAmount
-        shiftedPosition %= len(alphabet)
-        cipherText += alphabet[shiftedPosition]
-    print(f'Here is encoded result: {cipherText}')
+# def encrypt(originalText , shiftAmount):
+#     cipherText = ""
+#     for latter in originalText:
+#         shiftedPosition = alphabet.index(latter) + shiftAmount
+#         shiftedPosition %= len(alphabet)
+#         cipherText += alphabet[shiftedPosition]
+#     print(f'Here is encoded result: {cipherText}')
 
-encrypt(originalText = text , shiftAmount = shift)
+# encrypt(originalText = text , shiftAmount = shift)
 
 # to decrypt message
-def decrypt (originatText ,shiftAmount):
-    print()
+# def decrypt (originalText ,shiftAmount):
+#     outPut = ""
+#     for latter in originalText:
+#         shiftedPosition = alphabet.index(latter) - shiftAmount
+#         shiftedPosition %= len(alphabet)
+#         outPut += alphabet[shiftedPosition]
+#     print(f'Here is encoded result: {outPut}')
+
+# decrypt(originalText=text , shiftAmount=shift)    
+
+# converting both in single function 
+
+def  caeser (originalText , shiftAmount , encode_or_decode):
+    outPut = ""
+    for latter in originalText:
+        if encode_or_decode  == "decode":
+            shiftAmount *= -1
+        shiftedPosition = alphabet.index(latter) + shiftAmount
+        shiftedPosition %= len(alphabet)
+        outPut += alphabet[shiftedPosition]
+    print(f'Here is encoded result: {outPut}')
+
+caeser(text , shift , direction)
