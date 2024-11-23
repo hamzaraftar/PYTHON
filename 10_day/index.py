641d53b474e40e2ad006803da06058beb5670763
Hamza Asghar <hamzaraftar765@gmail.com>
2024-11-23 05:08:05 -0800

    function with output

---
+++ 10_day/index.py
@@ -0,0 +1,7 @@
+def my_funtion(fname , lname):
+    my_name = fname.title()
+    last_name = lname.title()
+    result = f'{my_name} {last_name}'
+    return result
+
+print(my_funtion("hamza" , "asghar"))
