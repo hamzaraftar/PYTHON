d397943f7b32f7341e79da8527bc2795d6612d58
Hamza Asghar <hamzaraftar765@gmail.com>
2024-11-24 05:06:10 -0800

    completing project of day12

--- 12_day/project.py
+++ 12_day/project.py
@@ -7,12 +7,15 @@ def deal_card():
 
 user_cards = []
 computer_cards = []
+is_game_over = False
 
 for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
+
 def calculate_score(cards):
+    """Take a list of cards and return  calculate from cards"""
     if sum(cards) == 21 and len(cards) == 2:
         return 0
     if 11 in cards and sum(cards) > 21:
@@ -20,4 +23,11 @@ def calculate_score(cards):
         cards.append(1)
     return sum(cards)
 
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
 
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
+else:
+    user_should_deal = input("Type 'y' to get another card , type 'n' pass: ")
+    
