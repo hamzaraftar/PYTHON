bf9c2ccd041e3dc0be27c8632597316dd64e8d98
Hamza Asghar <hamzaraftar765@gmail.com>
2024-11-26 05:36:52 -0800

    12 day project complete

--- 12_day/project.py
+++ 12_day/project.py
@@ -3,7 +3,6 @@ from random import randint
 EASY_LEVEL_TURNS = 10
 HARD_LEVEL_TURNS = 5
 
-
 def check_answer(user_guess , actual_answer):
     if user_guess > actual_answer:
         print("Too high.")
@@ -12,23 +11,25 @@ def check_answer(user_guess , actual_answer):
     else:
         print(f'You got it the answer is {actual_answer}')
 
-def check_difficulty():
+def set_difficulty():
     level = input("Choose the difficulty . Type 'easy' or 'hard': ")
     if level == "easy":
-        turns = EASY_LEVEL_TURNS
+        return  EASY_LEVEL_TURNS
     else:
-        turns = HARD_LEVEL_TURNS
-
-
-
+        return  HARD_LEVEL_TURNS
 
 
 # Choosing random number from 1 to 100
 print("Wellcome to random Guessing Game")
 print("I'm thinking of number between 1 and 100")
 answer = randint(1,100)
-
+print(f'past, the correct answer is {answer}')
 
 
 # Let the user  guess a numer
+
+turns  =set_difficulty()
+print(f'You have {turns} attempts remaining to guess the number ')
 guess =  int(input('Make the guess: '))
+
+check_answer(guess, answer)
