75626762704ae8a97656e6344edf5ba0afa550d5
Hamza Asghar <hamzaraftar765@gmail.com>
2024-12-28 03:48:27 -0800

    making turch of game

--- 23_day/main.py
+++ 23_day/main.py
@@ -9,12 +9,16 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(player.go_up , "Up")
-screen.onkey(player.go_down , "Down")
+
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
