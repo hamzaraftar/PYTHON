7c8d2b6937ba2d2f576a37b4d7e85d29a19aa05b
Hamza Asghar <hamzaraftar765@gmail.com>
2024-12-31 07:45:34 -0800

    putting states on screen dynamic

--- 25_day/us_states/main.py
+++ 25_day/us_states/main.py
@@ -7,13 +7,24 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-aswer_state = turtle.textinput(title="Guess the state" , prompt="What's another state's  name")
-print(aswer_state)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guess_state = []
+
+while len(guess_state) < 50:
+    answer_state = turtle.textinput(title=f"  {len(guess_state)}/50 State Correct" , prompt="What's another state's  name").title()
+    if answer_state in all_states:
+        guess_state.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]    
+        t.goto(state_data.x.item() , state_data.y.item())
+        t.write(answer_state)
 
 
-
-
-
+screen.exitonclick()
 
 
 
