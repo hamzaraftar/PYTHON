d2ebcaae42747922c7facb5ef489cbf701092b2b
Hamza Asghar <hamzaraftar765@gmail.com>
2024-12-31 08:22:55 -0800

    list comphersion

---
+++ 26_day/main.py
@@ -0,0 +1,6 @@
+numbers = [1,2,3]
+new_list = [n + 1 for n in numbers]
+# syntax new_list = [item for item in list]
+# print(new_list)
+range_list = [ num + 1 for num in range(1,5)]
+print(range_list)
