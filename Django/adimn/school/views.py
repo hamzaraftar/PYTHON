44197f4492786bedc98566b6e6be361bc97cec41
Hamza Asghar <hamzaraftar765@gmail.com>
2025-01-04 22:57:05 -0800

    djanog project

--- Django/adimn/school/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from school.models import Profile
-
-# Create your views here.
-def home(request):
-    stu = Profile.objects.all()
-    return render(request, 'index.html')
--- Django/project/vege/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-class Recepie(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True , blank=True)
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    image = models.ImageField(upload_to='recepie')
