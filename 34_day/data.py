90e4c84954ee1f5ab4717049eeb20a37f693afb1
Hamza Asghar <hamzaraftar765@gmail.com>
2025-01-06 07:42:20 -0800

    fix bug

--- 34_day/data.py
+++ 34_day/data.py
@@ -1,8 +1,11 @@
 import requests
-response = requests.get("https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=boolean")
 
+parameters = {
+    "amount": 10,
+    "type": "boolean",
+}
+
+response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
 question_data = data["results"]
-
-
