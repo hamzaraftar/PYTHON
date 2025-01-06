804b3b3ac019d2ad60efabba3ba59494427d0089
Hamza Asghar <hamzaraftar765@gmail.com>
2025-01-06 09:25:49 -0800

    showing user on screen on django

--- Django/proj/myapp/views.py
+++ Django/proj/myapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 def index(request):
-    return render(request, 'index.html' )
+    feature = Feature.objects.all()
+    return render(request, 'index.html',{'features' :feature } )
 
 def counter(request):
     return render(request, 'counter.html')
