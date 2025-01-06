b606f29c592a6d36500d5af634bacc15b15ab67c
Hamza Asghar <hamzaraftar765@gmail.com>
2025-01-06 07:20:29 -0800

    feateure of true or false

--- 34_day/ui.py
+++ 34_day/ui.py
@@ -15,11 +15,11 @@ class QuizInterface:
         self.canves.grid(row=1, column=0, columnspan=2, pady=50)
 
         true_image = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button = Button(image=true_image, highlightthickness=0 , command=self.turn_true)
         self.true_button.grid(row=2, column=0)
 
         false_image = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button = Button(image=false_image, highlightthickness=0 , command=self.turn_false)
         self.false_button.grid(row=2, column=1)
 
         self.get_next_question()
@@ -28,3 +28,11 @@ class QuizInterface:
     def get_next_question(self):
         q_text = self.quiz.next_question()
         self.canves.itemconfig(self.question_text, text=q_text) 
+
+    def turn_true(self):
+        self.quiz.check_answer("True")
+        self.get_next_question()
+
+    def turn_false(self):
+        self.quiz.check_answer("False")
+        self.get_next_question()    
