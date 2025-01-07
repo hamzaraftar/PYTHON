c6cdd4ae7ad5310304cdced8092e17fff8711d84
Hamza Asghar <hamzaraftar765@gmail.com>
2025-01-07 09:14:58 -0800

    fix

--- 35_day/main.py
+++ 35_day/main.py
@@ -1,11 +1,1 @@
-import requests
-api_key = "42205b7be76b414d497c3e44e81457a3"
-OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
-weather_params = {
-    "lat" : 31.561920,
-    "lon" : 74.348083,
-    "appid" : api_key,
-}
-response = requests.get(OWM_ENDPOINT, params=weather_params)
-data = response.json()
-print(data)
+print('ddddddd')
