ba346b0058b7bdd34292d5ce827b27e2eeb96bf0
Hamza Asghar <hamzaraftar765@gmail.com>
2025-01-07 10:23:56 -0800

    register route complete

--- Django/proj/myapp/urls.py
+++ Django/proj/myapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, counter
+from .views import index, counter ,register
 
 urlpatterns = [
     path('', index, name='index'),
     path('counter/', counter, name='counter'),
+    path('register/', register, name='counter'),
 ]
--- Django/proj/myapp/views.py
+++ Django/proj/myapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Feature
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 def index(request):
     feature = Feature.objects.all()
@@ -8,3 +10,24 @@ def index(request):
 
 def counter(request):
     return render(request, 'counter.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
+
+        if password == password2:
+            if User.objects.filter(email= email).exists():
+                messages.info(request , "Email already exist")
+                return redirect('register')
+            
+            elif User.objects.filter(username= username).exists():
+                messages.info(request , "User name  already in use")
+                return redirect('register')
+            
+            else:
+                user = User.objects.create_user(username=username , email=email , password=password)
+
+    return render(request, 'register.html')
