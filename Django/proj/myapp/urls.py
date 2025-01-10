46487ae64244518d0503ca5840ea0026f9cc166f
Hamza Asghar <hamzaraftar765@gmail.com>
2025-01-10 09:00:24 -0800

    formating latter

--- Django/proj/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index ,register ,login
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('register/', register, name='register'),
-    path('login/', login, name='login'),
-]
