2f82f5f80136e1c80f2d70603b83f7c9b34948a8
Hamza Asghar <hamzaraftar765@gmail.com>
2025-01-14 05:35:57 -0800

    intro to beautifull soup

---
+++ 45_day/bs4-start/main.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import lxml
+
+with open('website.html') as file:
+    content = file.read()
+
+soup = BeautifulSoup(content,"lxml")    
+# print(soup.title)
+print(soup.title.string)
