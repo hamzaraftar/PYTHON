efd328130bbae9f2fd6962bc649597f68c11deda
Hamza Asghar <hamzaraftar765@gmail.com>
2025-01-14 07:12:28 -0800

    scrpping Y combinator

--- 45_day/bs4-start/main.py
+++ 45_day/bs4-start/main.py
@@ -1,9 +1,26 @@
 from bs4 import BeautifulSoup
-import lxml
+import requests
 
-with open('website.html') as file:
-    content = file.read()
+response = requests.get('https://appbrewery.github.io/news.ycombinator.com/')
+yc_web_page = response.text
 
-soup = BeautifulSoup(content,"lxml")    
-# print(soup.title)
-print(soup.title.string)
+soup = BeautifulSoup(yc_web_page,"html.parser")
+
+articles = soup.find_all(name="a",class_="storylink")
+
+article_texts = []
+article_links = []
+
+for article_tag in articles:
+
+    text = article_tag.getText()
+    article_texts.append(text)
+
+    link = article_tag.get("href")
+    article_links.append(link)
+
+article_upvotes = [int(score.getText().split()[0])  for score in soup.find_all(name='span',class_="score")]
+
+print(article_texts)
+print(article_links)
+print(article_upvotes)
