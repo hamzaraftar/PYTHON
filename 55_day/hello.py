0c5aa7cefac91ff87acaa45c978cbb217e308127
Hamza Asghar <hamzaraftar765@gmail.com>
2025-02-14 05:55:47 -0800

    flask urls

---
+++ 55_day/hello.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route('/<name>')
+def great(name):
+    return f"Hello {name}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
