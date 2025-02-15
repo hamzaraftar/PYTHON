d8ccdaa76fc0e5330fdbbee1cf6390c7c6892466
Hamza Asghar <hamzaraftar765@gmail.com>
2025-02-15 00:39:50 -0800

    curd operatoin complete

--- Django/rest/DRF/gs9/api/views.py
+++ Django/rest/DRF/gs9/api/views.py
@@ -8,7 +8,8 @@ from .models import Student
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def studentapi(request,id=None):
-    if request.method == "GET":        
+    print(id)
+    if request.method == "GET":   
         if id is not None:
             stu = Student.objects.get(id=id)
             serializer = StudentSerializer(stu)
@@ -17,3 +18,26 @@ def studentapi(request,id=None):
         stu = Student.objects.all()
         serializer = StudentSerializer(stu, many=True)
         return Response(serializer.data)
+    
+    if request.method == "POST":
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(request.data)
+        return Response(serializer.errors)    
+
+    if request.method == "PUT":
+        stu = Student.objects.get(id=id)
+        serializer = StudentSerializer(stu, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+    if request.method == "DELETE":
+        stu = Student.objects.get(id=id)
+        stu.delete()
+        return Response({'msg':'Data Deleted'})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
