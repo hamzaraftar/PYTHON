085327958f08fd960e1de2a7f6a894207370c000
Hamza Asghar <hamzaraftar765@gmail.com>
2025-02-15 23:03:54 -0800

    clean sum bugs

--- Django/rest/DRF/G_VIEW_MIXI/api/views.py
+++ Django/rest/DRF/G_VIEW_MIXI/api/views.py
@@ -4,42 +4,27 @@ from .serializers import StudentSerializer
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin, RetrieveModelMixin
 
-class StudentList(GenericAPIView, ListModelMixin):
+class StudentList(GenericAPIView, ListModelMixin ,CreateModelMixin):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
     
-
-class StudentCreate(GenericAPIView, CreateModelMixin):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
     
 
-class StudentRetrive(GenericAPIView, RetrieveModelMixin):
+class StudentRetrive(GenericAPIView, RetrieveModelMixin ,UpdateModelMixin ,DestroyModelMixin):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
 
-
-class StudentUpdate(GenericAPIView, UpdateModelMixin):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
     
-
-class StudentUpdate(GenericAPIView, UpdateModelMixin):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
     def patch(self, request, *args, **kwargs):
         return self.partial_update(request, *args, **kwargs)
     
-class StudentDetele(GenericAPIView, DestroyModelMixin):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-    def put(self, request, *args, **kwargs):
+    def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
