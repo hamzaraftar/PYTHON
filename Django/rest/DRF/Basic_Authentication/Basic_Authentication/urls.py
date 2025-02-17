80cb5e1a6a86f9492ebc7c504f2d9ab89949952a
Hamza Asghar <hamzaraftar765@gmail.com>
2025-02-17 05:12:56 -0800

    simple auth in DRF

---
+++ Django/rest/DRF/Basic_Authentication/Basic_Authentication/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from api import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('student', views.StudentViewSet, basename='student')
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+]
