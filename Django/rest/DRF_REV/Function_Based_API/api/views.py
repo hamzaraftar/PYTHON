bd8831b40957f1af43ea865e2de6b3548bec4789
Hamza Asghar <hamzaraftar765@gmail.com>
2025-02-20 02:36:42 -0800

    understanign request.data

--- Django/rest/DRF_REV/Function_Based_API/api/views.py
+++ Django/rest/DRF_REV/Function_Based_API/api/views.py
@@ -3,6 +3,14 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-@api_view(["GET"])
+# @api_view(["GET"])
+# def hello_world(request):
+#     return Response({"message":"Hello world"})
+
+@api_view(["POST"])
 def hello_world(request):
-    return Response({"message":"Hello world"})
+    if request.method == "POST":
+        print(request.data.get("name"))
+        print(request.data.get("rollNo"))
+        print(request.data.get("age"))
+        return Response({"message":"Hello post request"})
