31fac2af162f3958841384c0288358b0e79fb69f
Hamza Asghar <hamzaraftar765@gmail.com>
2025-02-20 07:47:47 -0800

    making post request

--- Django/rest/DRF_REV/Function_Based_API/Function_Based_API/urls.py
+++ Django/rest/DRF_REV/Function_Based_API/Function_Based_API/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 from api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('student/', views.hello_world),
+    path('student/', views.get_data),
+    path('student/<int:id>/', views.get_data_by_id),
+    path('student/create/', views.create_student),
+    path('student/update/<int:id>/', views.update_student),
 ]
--- Django/rest/DRF_REV/Function_Based_API/api/views.py
+++ Django/rest/DRF_REV/Function_Based_API/api/views.py
@@ -1,16 +1,43 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework import status
 from rest_framework.response import Response
+from .serializers import StudentSerializer
+from .models import Student
+
+@api_view(["GET"])
+def get_data(request):
+    try:
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        return Response(serializer.data)
+    except:
+        return Response(serializer.error)
 
 
-# @api_view(["GET"])
-# def hello_world(request):
-#     return Response({"message":"Hello world"})
-
+@api_view(["GET"])
+def get_data_by_id(request,id):
+    try:
+        stu = Student.objects.get(id=id)
+        serializer = StudentSerializer(stu)
+        return Response(serializer.data)
+    except Student.DoesNotExist:
+        return Response({"error":"Student does't exist"})   
+    
 @api_view(["POST"])
-def hello_world(request):
-    if request.method == "POST":
-        print(request.data.get("name"))
-        print(request.data.get("rollNo"))
-        print(request.data.get("age"))
-        return Response({"message":"Hello post request"})
+def create_student(request):
+    try:
+        if request.method == "POST":            
+            serializer = StudentSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+
+            return Response(serializer.errors)
+    
+    except Exception as e:
+        return Response({"error": str(e)})   
+
+        
+
+        
