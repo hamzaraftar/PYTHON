3ef74c9dcef332c73004d092ec08e1446514b003
Hamza Asghar <hamzaraftar765@gmail.com>
2025-02-22 00:24:25 -0800

    viewset all operations

--- Django/rest/DRF_REV/ViewSet_/ViewSet_/urls.py
+++ Django/rest/DRF_REV/ViewSet_/ViewSet_/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from api import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('student',views.StudentApi, basename='student')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include(router.urls)),
 ]
--- Django/rest/DRF_REV/ViewSet_/api/views.py
+++ Django/rest/DRF_REV/ViewSet_/api/views.py
@@ -11,3 +11,47 @@ class StudentApi(viewsets.ViewSet):
             return Response(serializer.data)
         except Exception as e:
             return Response({'error':str(e)})
+
+    def retrieve(self,request,pk):
+        try:
+            stu = Student.objects.get(id=pk)
+            serializer = StudentSerializer(stu)
+            return Response(serializer.data)
+        except Exception as e:
+            return Response({'error':str(e)})
+        
+    def create(self,request):
+        try:
+            serializer = StudentSerializer(request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+        except  Exception as e:
+            return Response({'error':str(e)})
+
+    def update(self,request,pk):
+        try:
+            stu = Student.objects.get(id=pk)
+            serializer = StudentSerializer(request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+        except Exception as e:
+            return Response({'error':str(e)})
+
+    def partial_update(self,request,pk):
+        try:
+            stu = Student.objects.get(id=pk)
+            serializer = StudentSerializer(request.data , partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+        except Exception as e:
+            return Response({'error':str(e)})
+
+    def destory(self,request,pk):
+        try:
+            stu = Student.objects.get(id=pk)
+            stu.delete()
+        except   Exception as e:
+            return Response({'error':str(e)})  
