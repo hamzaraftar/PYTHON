4458fec281bc601fa2d4414667be0946b3ccd1a4
Hamza Asghar <hamzaraftar765@gmail.com>
2025-02-23 05:22:13 -0800

    custom permition class

--- Django/rest/DRF_REV/Custom_Permission/Custom_Permission/settings.py
+++ Django/rest/DRF_REV/Custom_Permission/Custom_Permission/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framrwork',
+    'rest_framework',
     'api',
 ]
 
--- Django/rest/DRF_REV/Custom_Permission/api/views.py
+++ Django/rest/DRF_REV/Custom_Permission/api/views.py
@@ -2,14 +2,14 @@ from .serializers import StudentSerializer
 from .models import Student
 from rest_framework import viewsets
 from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .custompermission import MyPermission
 
 class StudentModel(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    authentication_classes = [SessionAuthentication]
-    permission_classes=[IsAuthenticatedOrReadOnly]
-
+    authentication_classes = [ SessionAuthentication]
+    permission_classes = [MyPermission]
+    
 
 
 
