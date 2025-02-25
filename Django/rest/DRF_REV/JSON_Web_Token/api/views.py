5960b2c8e1a2f4c827651efd7005100314528a9d
Hamza Asghar <hamzaraftar765@gmail.com>
2025-02-24 23:50:38 -0800

    json web token implementation complete

--- Django/rest/DRF_REV/JSON_Web_Token/api/views.py
+++ Django/rest/DRF_REV/JSON_Web_Token/api/views.py
@@ -1,14 +1,14 @@
 from .serializers import StudentSerializer
 from .models import Student
 from rest_framework import viewsets
-from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class StudentModel(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    authentication_classes = [SessionAuthentication]
-    permission_classes=[IsAuthenticatedOrReadOnly]
+    authentication_classes = [JWTAuthentication]
+    permission_classes=[IsAuthenticated]
 
 
 
