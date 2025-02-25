37aa6687d54d84a1cbe8723de6c7ec053a3f8278
Hamza Asghar <hamzaraftar765@gmail.com>
2025-02-25 00:54:36 -0800

    throttling

--- Django/rest/DRF_REV/Throttling_/api/views.py
+++ Django/rest/DRF_REV/Throttling_/api/views.py
@@ -2,10 +2,12 @@ from .serializers import StudentSerializer
 from .models import Student
 from rest_framework import viewsets
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.throttling import AnonRateThrottle , UserRateThrottle
 
 class StudentModel(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     authentication_classes = [JWTAuthentication]
-    permission_classes=[IsAuthenticated]
+    permission_classes=[IsAuthenticatedOrReadOnly]
+    throttle_classes = [AnonRateThrottle ,UserRateThrottle]
