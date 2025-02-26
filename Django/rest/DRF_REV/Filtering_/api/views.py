2f8481c00d1d94a9cec34f717f0f8d1620675179
Hamza Asghar <hamzaraftar765@gmail.com>
2025-02-25 22:41:34 -0800

    filter package

--- Django/rest/DRF_REV/Filtering_/api/views.py
+++ Django/rest/DRF_REV/Filtering_/api/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from .serializers import StudentSerializer
 from .models import Student
 from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
 
 class StudentApi(ListAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['city']
 
