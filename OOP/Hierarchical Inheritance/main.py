e5c9482c98ca4658e039805a96df1b69fd739c37
Hamza Asghar <hamzaraftar765@gmail.com>
2025-03-02 22:55:45 -0800

    hirarical inheritance

---
+++ OOP/Hierarchical Inheritance/main.py
@@ -0,0 +1,32 @@
+# class Employee:
+#     def displa(self):
+#         print("this is employee class")
+
+# class Manager(Employee):
+#     def show(self):
+#         print("this is manager class ")
+
+#  Hierarchical Inheritance
+
+class Person:
+    def __init__(self, name , age):
+        self.name = name
+        self.age =  age
+    def display(self):
+        print('This peron ')
+
+class Employee(Person):
+    def __init__(self, sal ):
+        self.sal =sal
+        def display(self):
+            print('This Employee ')
+
+class Student(Person):
+    def __init__(self, name, age , marks):
+        super().__init__(name, age)
+        self.marks = marks
+        def display(self):
+            print('This Student ')
+
+s1 = Student('hamza', 12 , 333)
+e1 = 
--- OOP/main.py
+++
@@ -1,7 +0,0 @@
-class Employee:
-    def displa(self):
-        print("this is employee class")
-
-class Manager(Employee):
-    def show(self):
-        print("this is manager class ")
