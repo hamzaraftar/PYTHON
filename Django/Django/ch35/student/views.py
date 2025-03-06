8bf66c0dbb0f2bd99f028553916383b36914bb27
Hamza Asghar <hamzaraftar765@gmail.com>
2025-03-05 22:08:54 -0800

    Signup feature

--- Django/Django/ch35/student/views.py
+++ Django/Django/ch35/student/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from  django.contrib.auth.forms import UserCreationForm
+from  .forms import SignUpForm
 
 def sign_up(req):
     if req.method == "POST":   
-        fm = UserCreationForm(req.POST)
+        fm = SignUpForm(req.POST)
         if fm.is_valid():
             fm.save()
     else:
-        fm = UserCreationForm()
+        fm = SignUpForm()
     return render(req , 'signup.html',{"form":fm})
