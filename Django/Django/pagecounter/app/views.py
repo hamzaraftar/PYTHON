6561c57e215a29003b71db369af33e2961f5af94
Hamza Asghar <hamzaraftar765@gmail.com>
2025-03-11 09:25:53 +0500

    cahce in django

--- Django/Django/pagecounter/app/views.py
+++ Django/Django/pagecounter/app/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    ct = request.session.get('counter', 0)
+    new = ct + 1
+    request.session['counter'] = new
+    return render(request, 'home.html' , {'counter': new})
