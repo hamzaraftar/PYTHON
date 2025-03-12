d30e885201b535d8e3491c2c0a608dbff5edcfd8
Hamza Asghar <hamzaraftar765@gmail.com>
2025-03-12 10:38:13 +0500

    middlwares

---
+++ Django/Django/ch40/blog/middlewares.py
@@ -0,0 +1,9 @@
+def my_middlewares(get_response):
+    print('One time initialization')
+
+    def my_function(request):
+        print('This is before view')
+        response = get_response(request)
+        print('This is after view')
+        return response
+    return my_function
