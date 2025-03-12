004a838be7e7efa2fef43daab7cb1a1ff158e782
Hamza Asghar <hamzaraftar765@gmail.com>
2025-03-12 10:48:18 +0500

    class base middlwares

--- Django/Django/ch40/blog/middlewares.py
+++ Django/Django/ch40/blog/middlewares.py
@@ -1,9 +1,24 @@
-def my_middlewares(get_response):
-    print('One time initialization')
+# function base Middleware
 
-    def my_function(request):
-        print('This is before view')
-        response = get_response(request)
-        print('This is after view')
-        return response
-    return my_function
+# def my_middlewares(get_response):
+#     print('One time initialization')
+
+#     def my_function(request):
+#         print('This is before view')
+#         response = get_response(request)
+#         print('This is after view')
+#         return response
+#     return my_function
+
+# Class base Middleware
+
+class my_middlewares:
+    def __init__(self,get_response):
+        self.get_response = get_response
+        print('One time initialization')
+
+        def __call__(self,request):
+            print('This is before view')
+            response = self.get_response(request)
+            print('This is after view')
+            return response
