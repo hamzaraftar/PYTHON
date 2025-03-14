e4a313f4e4407cc6df956b2e853aa88315225e2a
Hamza Asghar <hamzaraftar765@gmail.com>
2025-03-14 11:03:50 +0500

    proxy model

--- Django/Django/ch42/blog/models.py
+++ Django/Django/ch42/blog/models.py
@@ -13,10 +13,26 @@ from django.db import models
 #     home_group = models.CharField(max_length=5)
 
 
-class ExamCenter(models.Model):
-    center_name = models.CharField(max_length=50)
-    city = models.CharField(max_length=70)
+# class ExamCenter(models.Model):
+#     center_name = models.CharField(max_length=50)
+#     city = models.CharField(max_length=70)
 
-class Student(ExamCenter):
-    name = models.CharField(max_length=40)
-    roll = models.IntegerField()
+# class Student(ExamCenter):
+#     name = models.CharField(max_length=40)
+#     roll = models.IntegerField()
+
+from django.db import models
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+
+class MyPerson(Person):
+    class Meta:
+        proxy = True
+
+    def do_something(self):
+       
+        pass
