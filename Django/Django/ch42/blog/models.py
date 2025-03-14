c00f0458ad779fa37229e1adf99711e52b37523c
Hamza Asghar <hamzaraftar765@gmail.com>
2025-03-14 10:47:03 +0500

    Multitable inheritance

--- Django/Django/ch42/blog/models.py
+++ Django/Django/ch42/blog/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
 
-class CommonInfo(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.PositiveIntegerField()
+# class CommonInfo(models.Model):
+#     name = models.CharField(max_length=100)
+#     age = models.PositiveIntegerField()
 
-    class Meta:
-        abstract = True
+#     class Meta:
+#         abstract = True
 
 
-class Student(CommonInfo):
-    home_group = models.CharField(max_length=5)
+# class Student(CommonInfo):
+#     home_group = models.CharField(max_length=5)
+
+
+class ExamCenter(models.Model):
+    center_name = models.CharField(max_length=50)
+    city = models.CharField(max_length=70)
+
+class Student(ExamCenter):
+    name = models.CharField(max_length=40)
+    roll = models.IntegerField()
