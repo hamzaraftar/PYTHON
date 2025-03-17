1ac6a5a3e215af2807109f2166437dea80a38063
Hamza Asghar <hamzaraftar765@gmail.com>
2025-03-17 20:56:53 +0500

    example

---
+++ OOP/example.py
@@ -0,0 +1,8 @@
+class Movie:
+    def __init__(self,title , mins ,hero):
+        self.title = title
+        self.runtime = mins
+        self.hero = hero
+
+    def display(self):
+        print(f"")
