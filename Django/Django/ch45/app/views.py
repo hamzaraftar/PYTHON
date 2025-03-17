60c4cdbae5c602f969ac73c41fe48cfcb038ba02
Hamza Asghar <hamzaraftar765@gmail.com>
2025-03-17 11:02:41 +0500

    example class base view

--- Django/Django/ch45/app/views.py
+++ Django/Django/ch45/app/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 
-def MyView(request):
+# function base view
+def myview(request):
     return HttpResponse("function base view")
+
+
+# class base veiw
+class Myview(View):
+    def get(self,request):
+        return HttpResponse('class base view')
--- Django/Django/ch45/ch45/urls.py
+++ Django/Django/ch45/ch45/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.MyView),
+    path('fun/', views.myview),
+    path('class/', views.Myview.as_view()),
 ]
