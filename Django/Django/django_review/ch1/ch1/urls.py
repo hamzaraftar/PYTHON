19a768e3b6443ef742415032aba2bb53284664ab
Hamza Asghar <hamzaraftar765@gmail.com>
2025-03-23 23:37:17 +0500

    add static file in project

--- Django/Django/django_review/ch1/ch1/urls.py
+++ Django/Django/django_review/ch1/ch1/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for pagecounter project.
+URL configuration for ch1 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -19,5 +19,5 @@ from django.urls import path
 from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path(""  ,views.home)
 ]
--- Django/Django/pagecounter/app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def home(request):
-    ct = request.session.get('counter', 0)
-    new = ct + 1
-    request.session['counter'] = new
-    return render(request, 'home.html' , {'counter': new})
