5f51f1745ae1903f3961bd7292f4fdda4b8997e5
Hamza Asghar <hamzaraftar765@gmail.com>
2025-03-23 20:54:48 +0500

    pandas intro

---
+++ pandas_tut/main.py
@@ -0,0 +1,8 @@
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+import pandas as pd
+
+data = pd.read_csv("weather_data.csv")
+print(data.temp)
