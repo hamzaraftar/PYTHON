634bc0c3a127ec7fe4bc097c163f74a707f62cc6
Hamza Asghar <hamzaraftar765@gmail.com>
2025-03-23 19:43:27 +0500

    iterater example

--- 56_day/ter.py
+++ 56_day/ter.py
@@ -1,3 +1,21 @@
-l = [10 , 20 , 30 , 40]
-iter_obj = iter(l)
-print(iter_obj)
+# l = [10 , 20 , 30 , 40]
+# iter_obj = iter(l)
+# print(iter_obj)
+
+class PowerOfTwo:
+    def __init__(self,max_value):
+        self.limit = max_value
+        self.current = 1
+        
+        
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        result = self.current
+        return result
+
+
+
+n = int(input("Enter the limit: "))
+iter_obj = PowerOfTwo(n)
