3d341ce3416f6c52166afba4b1b19b4c03f5f71d
Hamza Asghar <hamzaraftar765@gmail.com>
2025-03-25 11:42:09 +0500

    form in django display

--- Django/Django/django_review/ch2/ch2/urls.py
+++ Django/Django/django_review/ch2/ch2/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(''  , views.registration)
+    path(''  , views.registration , name='registration')
 ]
